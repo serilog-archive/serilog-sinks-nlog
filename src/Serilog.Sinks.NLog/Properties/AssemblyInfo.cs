4985b2852fc7e373c33e615146ebbc1f29464bd6
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2015-03-07 15:12:07 +1000

    Moved source from serilog/serilog (no build yet)

---
+++ src/Serilog.Sinks.NLog/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Serilog.Sinks.NLog")]
+[assembly: AssemblyDescription("Serilog sink for NLog")]
+[assembly: AssemblyCopyright("Copyright © Serilog Contributors 2014")]
